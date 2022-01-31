469edda5cee6ad8d1760a7d5e021095fb2e27eee
Mathieu DUBART <mathieudubart@MacBook-Pro-de-Mathieu.local>
2022-01-31 14:12:10 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+//importer les libs
+require('dotenv').config();
+const express = require('express');
+const axios = require('axios');
+
+//requipérer les variables ENV
+const app = express();
+app.set('view engine', 'ejs');
+const port = process.env.PORT;
+const accessToken = process.env.ACCESS_TOKEN;
+
+//set axios default url
+axios.defaults.baseURL = process.env.API_URL;
+
+//
+app.listen(port, () => {
+
+});
+
+app.get('/', (request, response)=>{
+
+    const userRequest = axios.get('/user', {
+        params: { access_token: accessToken }
+    }); //.then((apiResponse)=>{
+    //     console.log(request.query.user);
+    //     // response.send(apiResponse.data);
+    // });
+
+    const projectsRequest = axios.get('/user/shots', {
+        params: { access_token: accessToken, per_page: 100}
+    });
+
+    axios.all([userRequest, projectsRequest]).then(axios.spread((...apiResponses) => {
+        response.render('pages/index', {userInfo: apiResponses[0].data});
+    }));
+
+});
