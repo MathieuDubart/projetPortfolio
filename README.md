19755ff84ce9a523c070d14de46db0591990d546
Mathieu DUBART <mathieudubart@MacBook-Pro-de-Mathieu.local>
2022-02-11 11:00:10 +0100

    récupérer images et titre

---
+++ README.md
@@ -0,0 +1,26 @@
+``` DESCRIPTION ```
+
+
+
+
+
+
+``` CONTRAINTES DESIGNERS ```
+
+Nomenclature des fichiers/images: snakecase - nom_du_fichier.txt
+
+images: png
+grandes images sans transparence: jpeg/jpg
+logos: svg
+
+nombre couleurs: 4 couleurs
+
+[thumbnail = première image de la mosaïque] [projet = "nom_du_projet"] [ordre = x]
+[font_principale = lien google fonts] [font_secondaire = lien google fonts]
+[first_color = #color] [second_color = #color] 
+
+call-to-action: 
+
+``` CONTRAINTES DEVELOPPEURS ``` 
+
+Nomenclature des fichiers: snakecase - nom_du_fichier.txt
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const axios = require('axios');
 
-//requipérer les variables ENV
+//recupérer les variables ENV
 const app = express();
 app.set('view engine', 'ejs');
 const port = process.env.PORT;
@@ -12,7 +12,7 @@ const accessToken = process.env.ACCESS_TOKEN;
 //set axios default url
 axios.defaults.baseURL = process.env.API_URL;
 
-//
+
 app.listen(port, () => {
 
 });
@@ -24,17 +24,23 @@ app.get('/', (request, response)=>{
 
     const userRequest = axios.get('/user', {
         params: { access_token: accessToken }
-    }); //.then((apiResponse)=>{
+    });//.then((apiResponse)=>{
     //     console.log(request.query.user);
-    //     // response.send(apiResponse.data);
+    //     response.send(apiResponse.data);
     // });
 
     const projectsRequest = axios.get('/user/shots', {
         params: { access_token: accessToken, per_page: 100}
-    });
+    });//.then((apiResponse)=>{
+    //     console.log(request.query.user);
+    //     response.send(apiResponse.data);
+    // });
+
 
     axios.all([userRequest, projectsRequest]).then(axios.spread((...apiResponses) => {
-        response.render('pages/index', {userInfo: apiResponses[0].data});
+        response.render('pages/index', {userInfo: apiResponses[0].data,
+            userShots: apiResponses[1].data});
     }));
 
 });
+
